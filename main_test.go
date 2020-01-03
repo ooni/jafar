6f2719e719f41ce1ebb5ecdc287c81cc76a2e144
Simone Basso <bassosimone@gmail.com>
2020-01-03 14:56:47 +0100

    main: propagate child exit code on failure
    This allows to write tests where the child is, e.g., curl and be
    confident that the return code is the curl's return code.

--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
 
-import "os"
+	"github.com/ooni/jafar/shellx"
+)
 
 func TestIntegrationNoCommand(t *testing.T) {
 	*dnsProxyAddress = "127.0.0.1:0"
@@ -23,4 +27,49 @@ func TestIntegrationWithCommand(t *testing.T) {
 		*mainCommand = ""
 	}()
 	main()
+}
+
+func TestMustx(t *testing.T) {
+	t.Run("with no error", func(t *testing.T) {
+		var called int
+		mustx(nil, "", func(int) {
+			called++
+		})
+		if called != 0 {
+			t.Fatal("should not happen")
+		}
+	})
+	t.Run("with non-exit-code error", func(t *testing.T) {
+		var (
+			called   int
+			exitcode int
+		)
+		mustx(errors.New("antani"), "", func(ec int) {
+			called++
+			exitcode = ec
+		})
+		if called != 1 {
+			t.Fatal("not called?!")
+		}
+		if exitcode != 1 {
+			t.Fatal("unexpected exitcode value")
+		}
+	})
+	t.Run("with exit-code error", func(t *testing.T) {
+		var (
+			called   int
+			exitcode int
+		)
+		err := shellx.Run("curl", "-sf", "") // cause exitcode == 3
+		mustx(err, "", func(ec int) {
+			called++
+			exitcode = ec
+		})
+		if called != 1 {
+			t.Fatal("not called?!")
+		}
+		if exitcode != 3 {
+			t.Fatal("unexpected exitcode value")
+		}
+	})
 }
